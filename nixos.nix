ad76929a2bc7fcd15d0551086003069557257a5f
Stephane Lacoin (aka nxmatic) <stephane.lacoin@hyland.com>
2024-11-20 09:40:25 +0100

    nix builder

--- nixos.nix
+++ nixos.nix
@@ -9,8 +9,8 @@ let
 in 
 {
   imports = [
-    (modulesPath + "/profiles/qemu-guest.nix")
-    (modulesPath + "/profiles/nix-builder-vm.nix")
+#    (modulesPath + "/profiles/qemu-guest.nix")
+#    (modulesPath + "/profiles/nix-builder-vm.nix")
     ./systemd
   ];
 
@@ -107,54 +107,6 @@ in
 
   # Environment
   environment.systemPackages = with pkgs; [
-    bash
-    qemu
     yq-go
   ];
-
-  # Nix builder configuration
-  nix = {
-    buildMachines = [{
-      hostName = "localhost";
-      systems = [ "x86_64-linux" "aarch64-linux" ];
-      supportedFeatures = [ "kvm" "big-parallel" ];
-      maxJobs = 4;
-    }];
-    distributedBuilds = true;
-    extraOptions = ''
-        builders-use-substitutes = true
-      '';
-    channel.enable = lib.mkForce true;
-  };
-  documentation.enable = lib.mkForce true;
-  system = {
-    disableInstallerTools = lib.mkForce false;
-  };
-  virtualisation.fileSystems = {
-    "/etc/ssl/certs" = {
-      device = lib.mkForce "certs";
-      fsType = lib.mkForce "virtiofs";
-      options = lib.mkForce [ "ro"  ];
-    };
-    "/etc/ssl/cert.pem" = {
-      device = lib.mkForce "certs";
-      fsType = lib.mkForce "virtiofs";
-      options = lib.mkForce [ "ro"  ];
-    };
-    "/tmp/shared" = {
-      device = lib.mkForce "shared";
-      fsType = lib.mkForce "virtiofs";
-      options = lib.mkForce [ "rw"  ];
-    };
-    "/tmp/xchg" = {
-      device = lib.mkForce "xchg";
-      fsType = lib.mkForce "virtiofs";
-      options = lib.mkForce [ "rw"  ];
-    };
-    "/var/keys" = {
-      device = lib.mkForce "keys";
-      fsType = lib.mkForce "virtiofs";
-      options = lib.mkForce [ "rw"  ];
-    };
-  };
 }
